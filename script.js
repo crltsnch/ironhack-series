7a331804bf4ddb9aa4f7287e7460530fc54f6e9f
crltsnch <crltsnch@myuax.com>
2023-10-30 11:44:09 +0100

    bucle

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+
+seriesPending.push('Silicon Valley');
+
+for (let i = 0; i < seriesPending.length; i++) {
+  console.log(seriesPending[i]);
+}
